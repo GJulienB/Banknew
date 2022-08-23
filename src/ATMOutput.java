55e0643133c2b3119a11a9906639598318a4fd54
GJulienB <110825721+GJulienB@users.noreply.github.com>
2022-08-23 11:02:09 +0200

    Update ATMOutput.java

--- src/ATMOutput.java
+++ src/ATMOutput.java
@@ -1,18 +1,20 @@
-import java.util.Scanner;
 
 public class ATMOutput {
 
     public static void main(String[] args) {
-        CostumerDatabase.getCustomerData();
         startText();
         CustomerSecurity.insertCustomerNumber();
-        CustomerSecurity.insertPin();
+        Customer customer = CustomerSecurity.createCustomer(CustomerSecurity.validCustomerNumber);
+        CustomerSecurity.insertPin(customer);
+        Account account = new Account(customer.getBalance());
+        account.showMenu();
+
 
     }
 
     public static void startText() {
         System.out.println("B A N K O M A T EOS-Bank Standort: Hamburg");
-        System.out.print("Hallo, herzlich willkommen  Frau Wagner" + "! Bitte geben Sie Ihre Kundennummer an: ");
+        System.out.print("Hallo, herzlich willkommen" + "! Bitte geben Sie Ihre Kundennummer an: ");
     }
 
 }
